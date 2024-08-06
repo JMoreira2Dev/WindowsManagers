10ed63b8de4994742fc776226b2afb546fcbe48c
João Pedro Moreira <167461650+JMoreira2Dev@users.noreply.github.com>
2024-08-05 23:58:48 -0300

    .bat Operations

--- Batch Scripts/Script Backup Outlook.bat
+++ Batch Scripts/Script Backup Outlook.bat
@@ -1,22 +1,14 @@
 @echo off
-REM Fechar o Outlook caso esteja aberto
 taskkill /IM outlook.exe /F
 
-REM Defina os diretórios de origem e destino
 set "sourceDirectory=%USERPROFILE%\AppData\Local\Microsoft\Outlook"
 set "destinationDirectory=%CD%"
 
-REM Copie os arquivos, sobrescrevendo os existentes
 xcopy "%sourceDirectory%\*.pst" "%destinationDirectory%" /Y
 
-REM Verificar se a cópia foi bem-sucedida
 if %ERRORLEVEL% equ 0 (
-    REM Exibir uma mensagem de conclusão
     echo Backup was successful
-    
-    REM Desligar o computador após a cópia
 ) else (
-    REM Exibir uma mensagem de erro
     echo Backup failed
 )
 
